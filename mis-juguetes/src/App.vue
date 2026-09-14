4c789088986b9b394c3ce83cbde8f65f3bd9f5ac
ArturoDLopez <alandavidlopezduran053@gmail.com>
2026-09-14 17:22:39 -0600

    Se sube la carpeta de vue mis-juguetes

---
+++ mis-juguetes/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>
+        Proyectos personales by <strong>Arturo D. López</strong>
+    </h1>
+    <p>
+        En esta página web, se van a subir todas las ideas de programacion que alguna vez hayan cruzado por mi mente,
+        desde utilidades, juegos sencillo, hasta proyectos avanzados.
+    </p>
+</template>
